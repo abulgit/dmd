188faac420c8d50ddd7a3932435fd9f11f57bf49
abul <abulkhan19175@gmail.com>
2025-04-18 19:11:03 +0530

    error

--- compiler/test/fail_compilation/fix21167.d
+++ compiler/test/fail_compilation/fix21167.d
@@ -1,3 +1,11 @@
+/*
+TEST_OUTPUT:
+---
+fail_compilation/fix21167.d(13): Error: function `f` is not callable using argument types `(int, string, int)`
+fail_compilation/fix21167.d(15):        cannot pass argument `"foo"` of type `string` to parameter `int __param_1`
+fail_compilation/fix21167.d(9):        `fix21167.f(int __param_0, int __param_1, int __param_2)` declared here
+---
+*/
 void f(int, int, int){}
 
 void main()
